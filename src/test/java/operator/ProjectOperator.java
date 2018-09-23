59045bc430011c34525282deb151357816bf1bab
Jing Guo <jingguo@Jings-MacBook-Pro.local>
2018-09-23 01:17:56 -0400

    init change to project

--- src/test/java/operator/ProjectOperator.java
+++ src/test/java/operator/ProjectOperator.java
@@ -1,4 +1,30 @@
 package operator;
 
-public class ProjectOperator {
+import java.util.Map;
+
+public class ProjectOperator extends Operator {
+
+    Operator prevOperator;
+    Map<String, Integer> currentSchema;
+
+
+    @Override
+    public Tuple getNextTuple() {
+        return null;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void dump(int i) {
+
+    }
+
+    @Override
+    public Map<String, Integer> getSchema() {
+        return null;
+    }
 }
--- src/test/java/operator/ProjectOperatorTest.java
+++ src/test/java/operator/ProjectOperatorTest.java
@@ -1,5 +1,61 @@
+package operator;
+
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.statement.select.FromItem;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+import org.junit.Test;
+import util.Catalog;
+import util.SelectExpressionVisitor;
+
+import java.io.File;
+import java.io.StringReader;
+
 import static org.junit.Assert.*;
 
 public class ProjectOperatorTest {
 
+    @Test
+    public void getNextTuple() {
+
+        String tableName = "Sailors";
+        String tableFolder = "Samples/samples/input/db/data/";
+        File file = new File(tableFolder + tableName);
+
+        String statement = "SELECT * FROM Sailors S WHERE S.A <= 3;";
+        CCJSqlParserManager parserManager = new CCJSqlParserManager();
+        try{
+            PlainSelect plainSelect = (PlainSelect) ((Select) parserManager
+                    .parse(new StringReader(statement))).getSelectBody();
+            System.out.println("plainSelect: ");
+
+            Operator op = new ScanOperator(plainSelect, file);
+            Catalog catalog = Catalog.getInstance();
+            System.out.println(catalog.getCurrentSchema());
+            Tuple currentTuple = op.getNextTuple();
+            FromItem fromItem = plainSelect.getFromItem();
+            System.out.println("From item is " + fromItem);
+            Expression whereEx = plainSelect.getWhere();
+            System.out.println("Where expression is " + whereEx);
+            int index = op.getSchema().get("S.A");
+            System.out.println(index);
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void reset() {
+    }
+
+    @Test
+    public void dump() {
+    }
+
+    @Test
+    public void getSchema() {
+    }
 }
