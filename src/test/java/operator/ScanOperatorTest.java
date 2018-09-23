54e86733ea39d1ef99c361cac9ce5e3191d7c279
Xinhe <lixinhe126@126.com>
2018-09-22 21:58:20 -0400

    add schema in operator

--- src/test/java/operator/ScanOperatorTest.java
+++ src/test/java/operator/ScanOperatorTest.java
@@ -2,6 +2,11 @@ package operator;
 
 import org.junit.Test;
 import java.io.File;
+import java.io.StringReader;
+
+import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
 
 public class ScanOperatorTest{
 
@@ -11,10 +16,17 @@ public class ScanOperatorTest{
         String tableFolder = "Samples/samples/input/db/data/";
         File file = new File(tableFolder + tableName);
 
-        Operator op = new ScanOperator(file);
+        String statement = "SELECT * FROM Sailors, Reserves WHERE Sailors.A = Reserves.G;";
+        CCJSqlParserManager parserManager = new CCJSqlParserManager();
+        try{
+            PlainSelect plainSelect = (PlainSelect) ((Select) parserManager.parse(new StringReader(statement))).getSelectBody();
+            Operator op = new ScanOperator(plainSelect, file);
 
-        while(op.getNextTuple()!=null){
-            System.out.println("read successfully");
+            while(op.getNextTuple()!=null){
+                System.out.println("read successfully");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
         }
 
     }
