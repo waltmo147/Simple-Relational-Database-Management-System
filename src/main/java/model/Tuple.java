d3b45fb6393b645f608a54caa5801e0255514054
Xinhe <lixinhe126@126.com>
2018-09-27 19:34:06 -0400

    add output match test

--- src/main/java/model/Tuple.java
+++ src/main/java/model/Tuple.java
@@ -70,6 +70,6 @@ public class Tuple {
 
     @Override
     public String toString() {
-        return Arrays.toString(data).substring(1, Arrays.toString(data).length() - 1);
+        return Arrays.toString(data).substring(1, Arrays.toString(data).length() - 1).replaceAll("\\s", "");
     }
 }
--- src/test/java/com/sql/interpreter/AppTest.java
+++ src/test/java/com/sql/interpreter/AppTest.java
@@ -2,8 +2,35 @@ package com.sql.interpreter;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
+import java.io.*;
+
+import util.Catalog;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+    @Test
+    public void sqlResultMatch() throws Exception{
+        Handler.init();
+        Handler.parseSql();
+        for(int index = 1; index<=8; ++index){
+            File outfile = new File(Catalog.getInstance().getOutputPath() + String.valueOf(index));
+            File expectOutputfile = new File("Samples/samples/expected_output/" + "query" + String.valueOf(index));
+            BufferedReader br1 = new BufferedReader(new FileReader(outfile));
+            BufferedReader br2 = new BufferedReader(new FileReader(expectOutputfile));
+            String str1=br1.readLine(), str2=br2.readLine();
+            while(str1!=null && str2!=null){
+                Assert.assertEquals(str1, str2);
+                str1=br1.readLine();
+                str2=br2.readLine();
+            }
+            Assert.assertNull(str1);
+            Assert.assertNull(str2);
+            br1.close();
+            br2.close();
+        }
+    }
 }
